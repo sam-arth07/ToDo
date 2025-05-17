677bde45bfc640941e287641b1806a9a1c05cd2d
Samarth Chaplot <samarthchaplot7@gmail.com>
2025-05-17 20:00:22 +0530

    Inital Commit

---
+++ app/src/main/java/com/example/todo/data/ToDoDao.kt
@@ -0,0 +1,8 @@
+package com.example.todo.data
+
+import androidx.room.Dao
+
+@Dao
+interface ToDoDao {
+
+}
---
+++ app/src/main/java/com/example/todo/data/models/ToDoTask.kt
@@ -0,0 +1,14 @@
+package com.example.todo.data.models
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.todo.ui.util.Constants.DATABASE_TABLE_NAME
+
+@Entity(tableName = DATABASE_TABLE_NAME)
+data class ToDoTask(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val desc: String,
+    val priority: Priority,
+)
